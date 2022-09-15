e8758ee955822eba5c4ad60b017bdc22658bdca6
Joshua <93887296+joshuavanderbilt@users.noreply.github.com>
2022-09-15 10:54:08 -0500

    debugging

--- the.py
+++ the.py
@@ -172,11 +172,12 @@ async def lightshot(ctx, *args):
     arguments = ''.join(args)
     number = int(arguments)
     for number in range(0,30):
-        lslink = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
-        lstable = ["https://prnt.sc/", lslink]
-        lssend = ''.join(lstable)
-        await ctx.send(lssend)
-        print("User generated this link from LightShot:", lssend)
+        print(number)
+#        lslink = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
+#        lstable = ["https://prnt.sc/", lslink]
+#        lssend = ''.join(lstable)
+#        await ctx.send(lssend)
+#        print("User generated this link from LightShot:", lssend)
 
 
 @bot.command(help="Make the Joe Bot say what you want it to!")
