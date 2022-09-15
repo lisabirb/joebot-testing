cbf5e5e8b36b3cb35522f20b5eec3967e3f75883
Joshua <93887296+joshuavanderbilt@users.noreply.github.com>
2022-09-15 10:42:11 -0500

    undoing last thing

--- the.py
+++ the.py
@@ -86,9 +86,6 @@ async def superban(ctx, member : discord.Member):
         x = ''.join(thing)
         await ctx.send(x)
 
-# Alias
-sban = superban
-
 @bot.command(help="Takes away help from a member.")
 async def takehelp(ctx, member : discord.Member):
         thing = [member.mention, ' can no longer access the help channels.']
